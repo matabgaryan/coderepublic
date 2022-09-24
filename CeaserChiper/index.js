ecb03ccf2345c29a4235ea56d388ea6a3f9f5361
matabgaryan <matabgaryan1@gmail.com>
2022-09-24 16:28:24 +0400

    Ceasars Cipher

---
+++ CeaserChiper/index.js
@@ -0,0 +1,25 @@
+const FIRST_LETTER_ASCII_NUM = 65; // A
+const MID_LETTER_ASCII_NUM = 77; // M
+const MID_NEXT_LETTER_ASCII_NUM = 78; // N
+const LAST_LETTER_ASCII_NUM = 90; // M
+
+function caesarsCipher(str, shift) {
+    let result = "";
+    for (let i = 0; i < str.length; ++i){
+        let letter = str[i].toUpperCase();
+        const letterAsciiNum = letter.charCodeAt();
+        const isLetterInLeftSide = (letterAsciiNum > FIRST_LETTER_ASCII_NUM && letterAsciiNum <= MID_LETTER_ASCII_NUM);
+        const isLetterInRightSide = (letterAsciiNum > MID_NEXT_LETTER_ASCII_NUM && letterAsciiNum <= LAST_LETTER_ASCII_NUM);
+
+        if (isLetterInLeftSide){
+            result += String.fromCharCode(letterAsciiNum + shift);
+        }else if (isLetterInRightSide){
+            result += String.fromCharCode(letterAsciiNum - shift)
+        }else {
+            result += letter
+        }
+    }
+    return  result;
+}
+console.log(caesarsCipher("URYYB JBEYQ", 13)) // HELLO WORLD
+console.log(caesarsCipher("HELLO WORLD", 13)) // URYYB JBEYQ
